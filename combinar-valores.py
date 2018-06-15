782a3c4f99ab161b0b3faafb6d50003d8af5d1e1
marcostrfn <marcos.trfn@gmail.com>
2018-06-15 11:12:22 +0200

    Add files via upload

--- combinar-valores.py
+++ combinar-valores.py
@@ -22,6 +22,12 @@ import funciones.bolsa as fb
 def correlacion(VALORES, DIRECTORIO_BASE, TEMPORALIDAD, filename):
     
     
+    titulo = "{} - {} ( {} )".format(VALORES[0],VALORES[1],TEMPORALIDAD)
+    # every monday
+    mondays = MonthLocator()
+    daysFmt = DateFormatter("%d %b %y")
+
+
     quotes = pd.read_csv(filename,
                         index_col=0,
                         parse_dates=True,
@@ -29,7 +35,11 @@ def correlacion(VALORES, DIRECTORIO_BASE, TEMPORALIDAD, filename):
                         sep=";")
     
     # eliminar valores nulos por macd, emas, etc
-    quotes = quotes[100:]
+    
+    date1 = "2016-6-12 00:00"
+    date2 = "2018-6-12 23:00"
+
+    quotes = quotes[(quotes.index >= date1) & (quotes.index <= date2)]
     
     directorio_destino = os.path.join(DIRECTORIO_BASE, 'pares', TEMPORALIDAD)    
     if not os.path.exists(directorio_destino):
@@ -41,37 +51,40 @@ def correlacion(VALORES, DIRECTORIO_BASE, TEMPORALIDAD, filename):
     
     x = zip(date2num(quotes.index.to_pydatetime()))
 
-    plt.subplot2grid((8,5), (0,0), colspan=5, rowspan=5)
+    ax1 = plt.subplot2grid((8,5), (0,0), colspan=5, rowspan=5)
     
     
     # candlestick2_ohlc(ax,data_open[desde:],data_high[desde:],data_low[desde:],data_cierre[desde:],width=0.6)
     plt.plot(x, quotes['cierre'],'g',linewidth=3, label='DATA')
-    plt.plot(x, quotes['ema[5]'],'r', label='EMA5')
-    plt.plot(x, quotes['ema[20]'],'b', label='EMA20')
-    plt.plot(x, quotes['ema[50]'],'y', label='EMA50')
-    plt.title("{} ( {} )".format("titulo",TEMPORALIDAD),  fontsize=20)
+    plt.plot(x, quotes['ema[20]'],'r', label='EMA20')
+    plt.plot(x, quotes['ema[50]'],'b', label='EMA50')
+    plt.plot(x, quotes['ema[100]'],'y', label='EMA100')
+    plt.title("{}".format(titulo),  fontsize=20)
     
     plt.legend(loc="upper left")
     plt.grid(True)
-    v = (max(quotes['cierre']) - min(quotes['cierre'])) / 20.
-    plt.yticks(np.arange(min(quotes['cierre']), max(quotes['cierre'])+v, v))
+    
+    
+
     
     
     
        
-    plt.subplot2grid((8,5), (5, 0), colspan=5) # plt.subplot(5, 1, 1, rowspan=3)
+    ax2 = plt.subplot2grid((8,5), (5, 0), colspan=5) # plt.subplot(5, 1, 1, rowspan=3)
+    
     plt.plot(x, quotes['rsi14'],'g')
-    # plt.title("RSI14")
     plt.grid(True)
    
 
-    plt.subplot2grid((8,5), (6, 0), colspan=5) # plt.subplot(5, 1, 4)
-    plt.plot(x,quotes['esk14'], 'r')
-    plt.plot(x, quotes['esd14'], 'g')
+    ax3 = plt.subplot2grid((8,5), (6, 0), colspan=5) # plt.subplot(5, 1, 4)
+    
+    plt.plot(x,quotes['esk50'], 'r')
+    plt.plot(x, quotes['esd50'], 'g')
     plt.grid(True)
 
     
-    plt.subplot2grid((8,5), (7, 0), colspan=5) # plt.subplot(5, 1, 5)
+    ax4 = plt.subplot2grid((8,5), (7, 0), colspan=5) # plt.subplot(5, 1, 5)
+    
     plt.plot(x, quotes['macd'], 'r')
     plt.plot(x, quotes['macd_signal'], 'g')
 
@@ -79,6 +92,34 @@ def correlacion(VALORES, DIRECTORIO_BASE, TEMPORALIDAD, filename):
 
 
     
+    ax1.xaxis.set_major_locator(mondays)
+    ax1.xaxis.set_major_formatter(daysFmt)    
+    ax1.autoscale_view()
+    ax1.xaxis.grid(True, 'major')
+    ax1.grid(True)    
+
+    ax2.xaxis.set_major_locator(mondays)
+    ax2.xaxis.set_major_formatter(daysFmt)    
+    ax2.autoscale_view()
+    ax2.xaxis.grid(True, 'major')
+    ax2.grid(True)
+       
+    ax3.xaxis.set_major_locator(mondays)
+    ax3.xaxis.set_major_formatter(daysFmt)    
+    ax3.autoscale_view()
+    ax3.xaxis.grid(True, 'major')
+    ax3.grid(True)   
+
+    ax4.xaxis.set_major_locator(mondays)
+    ax4.xaxis.set_major_formatter(daysFmt)    
+    ax4.autoscale_view()
+    ax4.xaxis.grid(True, 'major')
+    ax4.grid(True)
+
+
+    fig.autofmt_xdate()
+
+
     
     
     valor1,valor2 = VALORES
@@ -110,8 +151,7 @@ def combinarValores(DIRECTORIO_BASE,VALORES,TEMPORALIDAD):
         print ("creando directorio.... {}".format(dirFilenameResult))
         
     filename3 = os.path.join(DIRECTORIO_BASE, 'csv', 'pares', TEMPORALIDAD, "{}-{}.csv".format(fname1,fname2))   
-    
-    
+
     file_1 = pd.read_csv(filename1,
                         index_col=0,
                         parse_dates=True,
@@ -160,7 +200,7 @@ def combinarValores(DIRECTORIO_BASE,VALORES,TEMPORALIDAD):
     rsi50 = fb.calcular_rsi(50, cierre)
        
     estocastico_sk_14, estocastico_sd_14 = fb.calcular_estocastico(cierre, high, low, 14, 3)
-    estocastico_sk_50, estocastico_sd_50 = fb.calcular_estocastico(cierre, high, low, 14, 3)    
+    estocastico_sk_50, estocastico_sd_50 = fb.calcular_estocastico(cierre, high, low, 20, 3)    
             
     #calcula emas de 5 a 200
     sma5 = fb.get_sma_periodo(5,cierre)
@@ -204,7 +244,7 @@ def main():
     config = ConfigParser.ConfigParser()
     config.read(configuracion)
     DIRECTORIO_BASE = config.get('data', 'directorio_base')
-    VALORES = ('EU50','US100')
+    VALORES = ('DE30','IBEX35')
     TEMPORALIDAD = 'D'
     combinarValores(DIRECTORIO_BASE,VALORES,TEMPORALIDAD)
     
