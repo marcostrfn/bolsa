8581ffddefecdea88e260a385bbb2d5f0685bcf3
marcostrfn <marcos.trfn@gmail.com>
2018-07-04 01:20:25 +0200

    graficar maximos y minimos

--- main.py
+++ main.py
@@ -23,50 +23,50 @@ if __name__ == '__main__':
     done = time.time()
     elapsed = done - start
     tiempos.append(('descargarDatos',elapsed))
-
-
+ 
+ 
     start = time.time()
     fd.preparar_datos_csv(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
     tiempos.append(('prepararDatos',elapsed))
-    
-    
+     
+     
     start = time.time()
     fd.calculo_mejor_valor(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
     tiempos.append(('calculo_mejor_valor', elapsed))
-    
-    
+     
+     
     start = time.time()
     fd.calculo_mejor_hora(obj_config,obj_csv,grafico=True)
     done = time.time()
     elapsed = done - start
     tiempos.append(('calculo_mejor_hora',elapsed))
-
-
+ 
+ 
     start = time.time()
     fd.calculo_soportes_resistencias(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
     tiempos.append(('calculo_soportes_resistencias',elapsed))
-
-
+ 
+ 
     start = time.time()
     fd.graficar_valores(obj_config)
     done = time.time()
     elapsed = done - start
     tiempos.append(('graficar_valores',elapsed))
-    
-
+     
+ 
     start = time.time()
     fd.graficar_valores_pares(obj_config)
     done = time.time()
     elapsed = done - start
     tiempos.append(('graficar_valores_pares',elapsed))
-    
-    
+     
+     
     start = time.time()
     procesar = obj_config.get_valores_calculo()
     for valor in procesar:
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     
     
     start = time.time()
-    fd.graficar_maximos_minimos(obj_config)
+    fd.graficar_maximos_minimos(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
     tiempos.append(('graficar_maximos_minimos',elapsed))
