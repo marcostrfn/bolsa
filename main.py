2ab2ae4480b63f32587fa8284888c292bd139952
marcostrfn <marcos.trfn@gmail.com>
2018-07-05 00:31:54 +0200

    optparse

--- main.py
+++ main.py
@@ -9,7 +9,31 @@ from funciones import reporting as fr
 from clases.Configuracion import Configuracion, CsvData
 import time
 
-    
+
+'''import optparse
+
+if __name__=="__main__":
+    parser = optparse.OptionParser("usage: %prog [options] arg1 arg2")
+    parser.add_option("-H", "--host", dest="hostname",
+                      default="127.0.0.1", type="string",
+                      help="specify hostname to run on")
+    parser.add_option("-p", "--port", dest="portnum", default=80,
+                      type="int", help="port number to run on")
+
+    (options, args) = parser.parse_args()
+    if len(args) != 2:
+        parser.error("incorrect number of arguments")
+    hostname = options.hostname
+    portnum = options.portnum
+	
+
+lista = ['limpiar_graficas','descargar_datos','preparar_datos_csv','calculo_mejor_valor',
+         'calculo_mejor_hora','calculo_mejor_valor','calculo_soportes_resistencias',
+         'graficar_valores','graficar_valores_pares','graficar_valor_medias',
+         'graficar_maximos_minimos','diferencia_cierre_horas','crear_reporte']
+
+'''
+
 
 if __name__ == '__main__':
     
@@ -29,14 +53,14 @@ if __name__ == '__main__':
     fd.descargar_datos(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
-    tiempos.append(('descargarDatos',elapsed))
+    tiempos.append(('descargar_datos',elapsed))
  
  
     start = time.time()
     fd.preparar_datos_csv(obj_config,obj_csv)
     done = time.time()
     elapsed = done - start
-    tiempos.append(('prepararDatos',elapsed))
+    tiempos.append(('preparar_datos_csv',elapsed))
      
      
     start = time.time()
@@ -102,6 +126,8 @@ if __name__ == '__main__':
     elapsed = done - start
     tiempos.append(('crear_reporte',elapsed))
 
+
     for t in tiempos:   
         print (t)
-
+        
+        
